7e42aca6435efee67ec6246c2a688e01568fa515
Prithik-Vijayakumar <vijayakumarprithik07@gmail.com>
2024-09-27 16:42:56 +0530

    Initialized the Front-Page

---
+++ src/Components/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Home.css";
+
+function Home () {
+
+    const handlelink = () => {
+        window.open ( "https://www.apple.com/", "_blank" )
+    }
+
+    return (
+        <>
+            <div className="container">
+                <nav className="nav-item-container">
+                    <img src={process.env.PUBLIC_URL + "/Images/iPhone 16 Pro/AppleLogo.png"} className="logo" onClick={handlelink}/>
+                    <img src={process.env.PUBLIC_URL + "/Images/iPhone 16 Pro/MenuIcon.png"} className="menu"/>
+                </nav>
+                <div className="home-img-container">
+                    <p className="heading">
+                        iPhone 16 Pro
+                    </p>
+                    <img src={process.env.PUBLIC_URL + "/Images/iPhone 16 Pro/Hello Image.jpg"} className="hello-img"/>
+                    <img src={process.env.PUBLIC_URL + "/Images/iPhone 16 Pro/Front Image.jpg"} className="home-img"/>
+                </div>
+                <div className="buy-btn-container">
+                    <button className="buy-btn">
+                        Buy
+                    </button>
+                </div>
+                <div className="detail-container">
+                    <p className="buy-detail-1">
+                        From $999 or $41.62/mo. for 24 mo.
+                    </p>
+                    <p className="buy-detail-2">
+                        Apple Intelligence coming this fall
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Home;
