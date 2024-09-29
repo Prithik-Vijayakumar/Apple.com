f2bfc4b9d31dd44e8b775e64a12089b81baf3bba
Prithik-Vijayakumar <vijayakumarprithik07@gmail.com>
2024-09-29 21:06:50 +0530

    Made enhancements in Carousel

--- src/Components/Highlights/Highlights.js
+++ src/Components/Highlights/Highlights.js
@@ -6,6 +6,7 @@ function Highlights () {
 
     const play = `${process.env.PUBLIC_URL}/Images/iPhone16Pro/Highlights/PlayIcon.png`;
     const stop = `${process.env.PUBLIC_URL}/Images/iPhone16Pro/Highlights/StopIcon.png`;
+    const playback = `${process.env.PUBLIC_URL}/Images/iPhone16Pro/Highlights/ReturnIcon.png`;
 
     const [activeIndex, setActiveIndex] = useState(0)
     const [autoPlay, setAutoPlay] = useState(true)
@@ -33,12 +34,21 @@ function Highlights () {
     }, [isVideoOpen])
 
     const handleIndex = (selectedIndex) => {
-        setTimeout(() => setActiveIndex(selectedIndex), 0)
+        setActiveIndex(selectedIndex)
+
+        if (activeIndex === 4) {
+            setTimeout (() => {setIndicatorImage(playback)}, 3000)
+        }
     }
 
     const handleAutoPlay = () => {
+        if (activeIndex !== 5) {
             setAutoPlay(!autoPlay)
             setIndicatorImage(indicatorImage === stop ? play : stop)
+        } else if (activeIndex === 5) {
+            setIndicatorImage(indicatorImage === playback ? stop : play)
+            setActiveIndex(0)
+        }
     }
 
     return (
@@ -75,7 +85,7 @@ function Highlights () {
                         </div>
                     )}
                     <div className="carousel">
-                    <Carousel activeIndex={activeIndex} onSelect={handleIndex} className="carousel" indicators= {false} interval={autoPlay ? 4000 : null}>
+                    <Carousel activeIndex={activeIndex} onSelect={handleIndex} className="carousel" indicators= {false} interval={autoPlay ? 4000 : null} wrap={false} >
                         <Carousel.Item>
                             <div className="hlts-container">
                                 <p className="hlts-details">
